b7728c139983f256698a203e713fcb7ddbfad24d
RidamAgrawal <agarwalagam04@gmail.com>
2026-03-15 21:13:23 +0530

    removing api.js from .gitignore

---
+++ api.js
@@ -0,0 +1,7 @@
+const APP_SCRIPT_BASE = "https://script.google.com/macros/s/";
+
+export const API = {
+  movies:      APP_SCRIPT_BASE + "__MOVIE_ID__/exec",
+  auth:        APP_SCRIPT_BASE + "__USERS_ID__/exec",
+  collections: APP_SCRIPT_BASE + "__COLLECTIONS_ID__/exec",
+};
